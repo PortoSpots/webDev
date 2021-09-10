c856c63ea2838853c4473eb2acd118673f50222f
wafflesgama <33865873+wafflesgama@users.noreply.github.com>
2021-09-10 22:30:24 +0100

    Project Bulk Commit
    Initial commit containing the Blazor project base

---
+++ PortoSpots/Services/ThemeProvider.cs
@@ -0,0 +1,27 @@
+﻿using MudBlazor;
+
+namespace PortoSpots.Services
+{
+    public static class ThemeProvider
+    {
+        public static MudTheme DefaultTheme = new MudTheme()
+        {
+            Palette = new Palette()
+            {
+                Primary = "#1F8BE9",
+                Secondary = "#FFA62B",
+                Tertiary = "#EDE7E3",
+                Dark = "#3E1929",
+                Surface = "#ffffff",
+                AppbarBackground = "#ffffff"
+            },
+
+            LayoutProperties = new LayoutProperties()
+            {
+                DrawerWidthLeft = "260px",
+                DrawerWidthRight = "300px"
+            },
+        };
+    }
+}
+
