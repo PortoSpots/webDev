03386445a80ee6357ff75dcc7eb81c6bd294ca8d
silval4 <lpcs3535@gmail.com>
2021-09-11 19:59:08 +0100

    Add base suggestion form

---
+++ Classes/Suggestion.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Suggestion
+{
+    [Required]
+    [StringLength(50, ErrorMessage = "Name is too long.")]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "Email is too long.")]
+    // todo email validation
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(1000, ErrorMessage = "SuggestionText is too long.")]
+    public string SuggestionText { get; set; }
+}
