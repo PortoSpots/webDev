1aac139889570ece4bea1b91414a815f05e48b8e
wafflesgama <33865873+wafflesgama@users.noreply.github.com>
2021-09-11 20:01:54 +0100

    Project Bulk Commit V2
    Second attempt to make the project template (pray emoji)

---
+++ Services/ThemeProvider.cs
@@ -0,0 +1,30 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace porto_spots.Services
+{
+    public static class ThemeProvider
+    {
+        public static MudTheme DefaultTheme = new MudTheme()
+        {
+            Palette = new Palette()
+            {
+                Primary = "#1F8BE9",
+                Secondary = "#FFA62B",
+                Tertiary = "#EDE7E3",
+                Dark = "#3E1929",
+                Surface = "#ffffff",
+                AppbarBackground = "#ffffff"
+            },
+
+            LayoutProperties = new LayoutProperties()
+            {
+                DrawerWidthLeft = "260px",
+                DrawerWidthRight = "300px"
+            },
+        };
+    }
+}
