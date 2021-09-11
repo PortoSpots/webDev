12c47433cee32bae30f42cdc1963ee22e6b32c9f
wafflesgama <33865873+wafflesgama@users.noreply.github.com>
2021-09-11 20:19:59 +0100

    Project Bulk Commit V2.1
    Second attempt to make the project template (pray emoji)

--- Domain/Suggestion.cs
+++ Domain/Suggestion.cs
@@ -4,14 +4,14 @@ public class Suggestion
 {
     [Required]
     [StringLength(50, ErrorMessage = "Name is too long.")]
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     [Required]
     [StringLength(100, ErrorMessage = "Email is too long.")]
     // todo email validation
-    public string Email { get; set; }
+    public string? Email { get; set; }
 
     [Required]
     [StringLength(1000, ErrorMessage = "SuggestionText is too long.")]
-    public string SuggestionText { get; set; }
+    public string? SuggestionText { get; set; }
 }
